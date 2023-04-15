01a7548b9cc2e1c3a42a203cc6f16ce105fa9eb6
ABDURAHIMAN K <laluvga@gmail.com>
2023-04-15 09:01:55 +0530

    Stage 3/5: Working on the gameplay

--- stages.py
+++ stages.py
@@ -1,5 +1,11 @@
-# Stage 2/5: New rules
+# Stage 3/5: Working on the gameplay
 no_pencil = int(input("How many pencils would you like to use:\n"))
 player = input("Who will be the first (John, Jack):\n")
-print("|" * no_pencil)
-print(f"{player} is going first!")
+while no_pencil > 0:
+    print("|" * no_pencil)
+    pencils = int(input(f"{player}'s turn:\n"))
+    no_pencil -= pencils
+    if player == "John":
+        player = "Jack"
+    else:
+        player = "John"
