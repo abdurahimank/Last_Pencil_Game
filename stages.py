46f2cf5e355e97f95fdd43f31b04b51b3bf4277c
ABDURAHIMAN K <laluvga@gmail.com>
2023-05-10 10:24:55 +0530

    Stage 4/5: Fair play

--- stages.py
+++ stages.py
@@ -1,11 +1,47 @@
-# Stage 3/5: Working on the gameplay
-no_pencil = int(input("How many pencils would you like to use:\n"))
-player = input("Who will be the first (John, Jack):\n")
+# Stage 4/5: Fair play
+import random
+
+
+print("How many pencils would you like to use:")
+while True:
+    no_pencil = input()
+    if not no_pencil.isdigit():
+        print("The number of pencils should be numeric")
+    elif no_pencil == "0":
+        print("The number of pencils should be positive")
+    else:
+        break
+print("Who will be the first (John, Jack)")
+while True:
+    player = input()
+    if player in ["John", "Jack"]:
+        break
+    print("Choose between John and Jack")
+no_pencil = int(no_pencil)
 while no_pencil > 0:
     print("|" * no_pencil)
-    pencils = int(input(f"{player}'s turn:\n"))
-    no_pencil -= pencils
     if player == "John":
         player = "Jack"
-    else:
+        print("John's turn!")
+        while True:
+            pencil_taken = input()
+            if pencil_taken not in ["1", "2", "3"]:
+                print("Possible values: '1', '2' or '3'")
+            elif int(pencil_taken) > no_pencil:
+                print("Too many pencils were taken")
+            else:
+                break
+    elif player == "Jack":
         player = "John"
+        print("Jack's turn:")
+        while True:
+            pencil_taken = input()
+            if pencil_taken not in ["1", "2", "3"]:
+                print("Possible values: '1', '2' or '3'")
+            elif int(pencil_taken) > no_pencil:
+                print("Too many pencils were taken")
+            else:
+                break
+    no_pencil -= int(pencil_taken)
+
+print(f"{player} won!")
